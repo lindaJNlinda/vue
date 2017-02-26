0e9f4ba719e7eb133ba1a5a4a40a9f7d01be4692
linda <1016799811@qq.com>
2017-02-26 18:59:10 +0800

    test

---
+++ src/module/login/login.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './app'
+import qiniu from 'qiniu.js'
+
+
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  components: { App }
+})
---
+++ src/module/login/test.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>uuuuuuuuuuuuuuuuuu</p>
+</template>
